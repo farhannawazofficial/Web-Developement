114aba2c7e11dc7b0296b833234a14236438d19d
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2024-11-23 09:37:00 +0500

    new

--- Fronted/JavaScript Classes/script.js
+++ Fronted/JavaScript Classes/script.js
@@ -4,3 +4,22 @@
 // 1- Let 
 // 2- Conts
 // 3 - var
+
+//Data Type 
+// JavaScript has 8 Datatypes
+
+// 1- String
+// 2- Number
+// 3- Bigint
+// 4- Boolean
+// 5- Undefined
+// 6- Null
+// 7- Symbol
+// 8- Object
+
+// The Object Datatype
+// The object data type can contain both built-in objects, and user defined objects:
+
+// Built-in object types can be:
+
+// objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.
