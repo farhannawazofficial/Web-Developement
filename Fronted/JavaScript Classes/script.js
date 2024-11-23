5b44ff691f521f8b0ba34ffb41bda594ee9a59cd
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2024-11-23 09:57:39 +0500

    Update script.js

--- Fronted/JavaScript Classes/script.js
+++ Fronted/JavaScript Classes/script.js
@@ -29,20 +29,32 @@
 // Numbers:
 let length = 16;
 let weight = 7.5;
+console.log(length + weight)
+console.log(typeof length, typeof weight)
 
 // Strings:
 let color = "Yellow";
 let lastName = "Johnson";
+console.log(color + weight)
+console.log(typeof color, typeof lastName)
 
 // Booleans
 let x = true;
 let y = false;
+console.log(x + y)
+console.log(typeof x, typeof y)
 
 // Object:
 const person = {firstName:"John", lastName:"Doe"};
+console.log(person)
+console.log(typeof person)
 
 // Array object:
 const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars)
+console.log(typeof cars)
 
 // Date object:
 const date = new Date("2022-03-25");
+console.log(date)
+console.log(typeof data)
