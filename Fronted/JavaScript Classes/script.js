965409faba2dab3459fd01b92cb144955526041d
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2024-11-23 09:37:47 +0500

    Update script.js

--- Fronted/JavaScript Classes/script.js
+++ Fronted/JavaScript Classes/script.js
@@ -23,3 +23,26 @@
 // Built-in object types can be:
 
 // objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.
+
+// Example
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
