662e3a132ede5a7ecf60564b0fbec833bc42299b
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2024-11-23 10:04:14 +0500

    Update script.js

--- Fronted/JavaScript Classes/script.js
+++ Fronted/JavaScript Classes/script.js
@@ -58,3 +58,16 @@ console.log(typeof cars)
 const date = new Date("2022-03-25");
 console.log(date)
 console.log(typeof data)
+
+//Object
+let o = {
+    "name": "Farhan",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
